9c519f4a59c231fa0c64c49117a6be67a3146af3
Assen Kolov <assen.kolov@nl.abnamro.com>
2016-09-15 13:51:21 +0200

    idea

---
+++ equip_idea_ide.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+#
+# Ubuntu Equip 
+# IntelliJ Idea IDE (latest version https://download.jetbrains.com/idea/ideaIC-2016.2.4.tar.gz)
+# Licence: Apache 2.0
+# to run: wget --no-check-certificate https://download.jetbrains.com/idea/ideaIC-2016.2.4.tar.gz && bash equip_idea_ide.sh
+#                                    &&
+
+if [ -d "/opt/idea" ]; then
+	echo "Idea already installed in /opt/idea, skipping!"
+	rm -f equip_idea_ide.sh
+	exit 0
+fi
+
+VERSION="ideaIC-2016.2.4.tar.gz"
+LINK="https://download.jetbrains.com/idea/$VERSION"
+
+wget --no-check-certificate "$LINK"
+mv $Version idea.tar.gz
+tar -xvf idea.tar.gz
+
+# Will copy to /opt
+sudo mv idea /opt/
+
+# Cleanup
+rm -f idea.tar.gz
+rm -f equip_idea_ide.sh
+
+#Create link on the PATH
+sudo ln -s /opt/idea/bin/idea.sh /usr/local/bin/idea
+
+echo "Installed in /opt/idea"
